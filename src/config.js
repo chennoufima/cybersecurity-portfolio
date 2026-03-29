a1ae324d8e00e2483bc6a19a8cd940694792791d
chennoufima <134719790+chennoufima@users.noreply.github.com>
2026-03-29 12:06:49 +0100

    Update email and social media links in config

--- src/config.js
+++ src/config.js
@@ -1,27 +1,17 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'chennoufima@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/chennoufima',
     },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
-    },
+    
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/mohamed-aymen-chennoufi-knowledgemanager-servicedesk-itilv4',
     },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
+    
   ],
 
   navLinks: [
